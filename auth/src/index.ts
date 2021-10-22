401b927ae4bd9447a1990905d12a7f3f9bc8f60c
rajapattanayak <pattanayak.raja@gmail.com>
2021-10-22 20:40:31 +0530

    auth with skaffold

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,19 @@
-import express from 'express'
-import { json } from 'body-parser'
+import express from 'express';
+import { json } from 'body-parser';
 
-const app = express()
-app.use(json())
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
-app.get('/api/users/currentuser', (req, res) => {
-    console.log('Debug1')
-    res.send('Hi there !')
-})
+const app = express();
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
-    console.log('Listening auth service on port 3000 !')
-})
+  console.log('Listening on port 3000!!!!!!!!');
+});
